45b182e913ce0ca870b9e5e2c87832bbb47b734c
Matheus Ribeiro <mathribe2020@gmail.com>
2020-03-25 22:19:41 -0300

    Alterações de arquivos base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,18 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+
+import './global.css';
+
+import Logon from './pages/Logon'; //não é necessário importar o index.js, já que ele será o primeiro a ser procurado na pasta
+
+//Dica: em toda aplicação web, criar um arquivo de CSS global, global.css
 
 //Quando o HTML estiver dentro de uma função JavaScript, ele é chamado de JSX:
 //  JSX = (JavaScript XML)
 //Componente básico:
 function App() {
-
-  //[counter,] terá o valor da posição [0]
-  //[, setCounter] -> função responsável por alterar o valor de counter
-  const [counter, setCounter] = useState(0); //variável do tipo LET (para poder ser alterada), ao invés de uma CONST
-  //o método useState() retorna um array com 2 posições:
-  //  [valor, funcaoDeAtualizacaoDesseValorOriginal] 
-
-  function increment(){
-    setCounter(counter + 1);
-    //nesse caso, apenas aparecerá o resultado no console, o elemento da interface não será alterado, pois não estamos usando um ESTADO
-  }
-
-  //Estado em React: toda vez que o estado é alterado, será remontado (renderizado novamente) o componente, exibindo as novas informações em tela;
-    
+  //Estado em React: toda vez que o estado é alterado, será remontado (renderizado novamente) o componente, exibindo as novas informações em tela;    
   return (
-    <div>
-    <Header>Contador: {counter}</Header> 
-    <button onClick= {increment}>Incrementar</button>    
-
-    </div>
-    //utilizando o conceito de propriedades para os componentes. Nesse caso, neste arquivo, o Header é um componente externo (de outro arquivo js)
+    <Logon/>
   );
 }
 
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import './styles.css'; //usando o './' para referenciar um arquivo na mesma pasta (diretório); '../' para voltar uma pasta 
+//Para importar o pacote de ícones desejado:
+//utizar as chaves {} para importar ícones específicos (fezendo a desestruturação)
+import { FiLogIn } from 'react-icons/fi'; //feather-icons //CTRL + SPACE = para abrir a caixa de opções dentro das chaves {};
+//os ícones componente aceitam como props size e color;
 
+import './styles.css'; //usando o './' para referenciar um arquivo na mesma pasta (diretório); '../' para voltar uma pasta 
+//Para utilizar imagens e svg, iremos importá-las também:
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
 export default function Logon(){
     return(
-        <h1>Hello World</h1>
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+                <form>
+
+                    <h1>Faça seu Logon</h1>
+                    <input placeholder="Sua ID"/>
+                    <button type="submit">Entrar</button>
+
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041" /> 
+                        Não tenho cadastro
+                    </a>
+                </form>
+
+            </section>
+
+            <img src={heroesImg} alt="Heroes"/>
+        </div>
     );
 }
