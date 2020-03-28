615f1cc3c9d51606e3239e1f86865c3a49ebf500
Matheus Ribeiro <mathribe2020@gmail.com>
2020-03-28 04:57:00 -0300

    Mobile App

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useNavigation} from '@react-navigation/native';
+import {useNavigation, useRoute } from '@react-navigation/native';
 import * as MailComposer from 'expo-mail-composer';
 import { View, Text, Image, TouchableOpacity, Linking} from 'react-native';
 import { Feather, FontAwesome5 } from '@expo/vector-icons';
@@ -10,8 +10,10 @@ import styles from './styles';
 
 export default function Detail(){
 
+    const route = useRoute();
     const navigation = useNavigation();
-    const message = 'Olá, APAD! Estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada", com o valor de R$120,00.';
+    const incident = route.params.incident;
+    const message = `Olá, ${incident.name}! Estou entrando em contato pois gostaria de ajudar no caso "${incident.title}", com o valor de ${Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(incident.value)}.`;
     function navigateBack(){
         navigation.navigate('Incidents');
     };
@@ -19,15 +21,15 @@ export default function Detail(){
     //Função para envio de E-mail pelo aplicativo:
     function sendMail() {
         MailComposer.composeAsync({
-            subject: 'Herói do caso: Cadelinha atropelada',
-            recipients: ['diego@rocketseat.com.br'],
+            subject: `Herói do caso: ${incident.title}`,
+            recipients: [incident.email],
             body: message,
         })
     };
     //Função para contato por WhatsApp:
     //Para abrir aplicações externas em um dispositivo móvel, utiliza-se uma tecnologia chamada "Deep-Linking" que está presente dentro do React Native também  
     function sendWhatsApp() {
-        Linking.openURL(`whatsapp://send?phone=5514997080544&text=${message}`);
+        Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`);
     };
 
     return(
@@ -41,13 +43,17 @@ export default function Detail(){
 
             <View style={styles.incident}>
                 <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-                <Text style={styles.incidentValue}>APAD</Text>
+                <Text style={styles.incidentValue}>{incident.name} ({incident.city} - {incident.uf})</Text>
 
-                <Text style={styles.incidentProperty}>CASO:</Text>
-                <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
-
+                <Text style={styles.incidentProperty}>DESCRIÇÃO:</Text>
+                <Text style={styles.incidentValue}>{incident.description}</Text>
                 <Text style={styles.incidentProperty}>VALOR:</Text>
-                <Text style={styles.incidentValue}>R$ 120,00</Text>
+                    <Text style={styles.incidentValue}>
+                        {Intl.NumberFormat('pt-BR', { 
+                            style: 'currency', 
+                            currency: 'BRL' 
+                        }).format(incident.value)}
+                    </Text>
             </View>
 
             <View style={styles.contactBox}>
--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {useNavigation} from '@react-navigation/native';
 import { View, FlatList, Image, Text, TouchableOpacity} from 'react-native';
 //Para Listas de items no React Native em que precisam de um scroll, utilizar ao invés de uma View, uma FlatList
 //Componente button já vem com estilização padrão do sistema operacional;
 //TouchableOpacity: torna qualquer coisa clicável e ao clicar sua opacidade será reduzida;
+//useEffect -> para carregar uma informação assim que o componente é exibido em tela
 import { Feather } from '@expo/vector-icons';
 import logoImg from '../../assets/logo.png';
-
 import styles from './styles';
+import api from '../../services/api';
 export default function Incidents(){
 
     const navigation = useNavigation();
-
-    function navigateToDetail(){
-        navigation.navigate('Detail');
+    function navigateToDetail(incident){
+        navigation.navigate('Detail', { incident });
     };
+
+    const [incidents, setIncidents] = useState([]);
+    const [total, setTotal] = useState(0); //valor de início
+    const [page, setPage] = useState(1); //valor de início
+    const [loading, setLoading] = useState(false); //para armazenar uma informação quando estiver buscando dados novos, para evitar que estes dados sejam buscados novamente
+
+    async function loadIncidents(){
+
+        if (loading) {
+            return;
+        }
+
+        if (total > 0 && incidents.length == total) { //se todas as informações estiverem carregadas, não irá buscar mais nada;
+            return;
+        }
+
+        setLoading (true); 
+
+        const response = await api.get('/incidents', {
+            params: { page }
+        });
+        //copiando todos os valores já existentes no estado incidents, e copiando todos os valores de response.data
+        setIncidents([... incidents, ... response.data]); //anexando 2 arrays dentro de um único array
+        setTotal(response.headers['x-total-count']);
+        setPage(page + 1);
+        setLoading (false);
+
+    };
+
+    useEffect(() => {
+        loadIncidents();
+    }, []);
 
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg}/>
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0 casos</Text>
+                    Total de <Text style={styles.headerTextBold}>{total} casos</Text>
                 </Text>
             </View>
 
@@ -30,23 +62,30 @@ export default function Incidents(){
 
             <FlatList 
                 style={styles.incidentList} 
-                data={[ 1, 2, 3, 4, 5, 6]}
-                keyExtractor={incident => String(incident)} //função irá receber cada um dos incidents e retornará as informações únicas do array de DATA[];
+                data={incidents}
+                keyExtractor={incident => String(incident.id)} //função irá receber cada um dos incidents e retornará as informações únicas do array de DATA[];
                 showsVerticalScrollIndicator={false}
-                renderItem={() => (
+                onEndReached={loadIncidents} //ao final da lista
+                onEndReachedThreshold={0.2} //quantos % do final da lista o usuário deve estar situado no scroll para que carregue novos items
+                renderItem={({ item: incident}) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
-                        <Text style={styles.incidentValue}>APAD</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
 
                         <Text style={styles.incidentProperty}>CASO:</Text>
-                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
                         <Text style={styles.incidentProperty}>VALOR:</Text>
-                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+                        <Text style={styles.incidentValue}>
+                            {Intl.NumberFormat('pt-BR', { 
+                                style: 'currency', 
+                                currency: 'BRL' 
+                            }).format(incident.value)}
+                        </Text>
 
                         <TouchableOpacity 
                             style={styles.detailsButton}
-                            onPress={navigateToDetail}
+                            onPress={() => navigateToDetail(incident)} //retornando parâmetro, usando arrow function
                         >
                             <Text style={styles.detailsButtonText}> Ver mais detalhes </Text>
                             <Feather name="arrow-right" size={32} color="#E02041"/>
