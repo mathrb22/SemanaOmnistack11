4c3df4eb01ac5b0992303b71d199ae3acd5b1c1a
Matheus Ribeiro <mathribe2020@gmail.com>
2020-10-12 16:33:48 -0300

    Update Logon Index.js - Corrige os comentários

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,30 +1,30 @@
 import React, { useState } from 'react';
 
-import { Link, useHistory } from 'react-router-dom'; //trocar as âncoras em HTML por <Link/>, e os href="" por to=""
+import { Link, useHistory } from 'react-router-dom'; //Trocar as âncoras em HTML por <Link/>, e os href="" por to=""
 //Para importar o pacote de ícones desejado:
-//utizar as chaves {} para importar ícones específicos (fezendo a desestruturação)
-import { FiLogIn } from 'react-icons/fi'; //feather-icons //CTRL + SPACE = para abrir a caixa de opções dentro das chaves {};
-//os ícones componente aceitam como props size e color;
+//Utilizar as chaves {} para importar ícones específicos (fezendo a desestruturação)
+import { FiLogIn } from 'react-icons/fi'; //Feather-icons //CTRL + SPACE = para abrir a caixa de opções dentro das chaves {};
+//Os ícones componentes aceitam como props size e color;
 
 import api from '../../services/api';
 
-import './styles.css'; //usando o './' para referenciar um arquivo na mesma pasta (diretório); '../' para voltar uma pasta 
+import './styles.css'; //Usando o './' para referenciar um arquivo na mesma pasta (diretório); '../' para voltar uma pasta 
 //Para utilizar imagens e svg, iremos importá-las também:
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
-//não utilizar class na propriedade pois class é uma palavra reservada do JavaScript
+//Não utilizar class na propriedade pois class é uma palavra reservada do JavaScript
 export default function Logon(){
 
     const [id, setId] = useState('');
     const history = useHistory();
 
     async function handleLogin(e) {
-        e.preventDefault(); //fazer isso em todo formulário no React
+        e.preventDefault(); //Fazer isso em todo formulário no React
 
         try {
             const response = await api.post('sessions', { id });
-            //como os dados precisam estar visíveis por toda aplicação, aplicar no localStorage do navegador:
+            //Como os dados precisam estar visíveis por toda aplicação, aplicar no localStorage do navegador:
             localStorage.setItem('ongId', id);
             localStorage.setItem('ongName', response.data.name);
             history.push('/profile');
